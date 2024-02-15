3692d039add6cb3945c4928e2467cb8abd0946d3
Michael Bradley <michaelmbrad@gmail.com>
2024-02-14 21:22:20 -0500

    Update invisibility rules when steam is launched
    Maybe go further with forcergbx or whatever it's called?

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -6,6 +6,8 @@
 # Effects makes things look nicer normally, but messes with game windows and reduces performance
 hyprctl --batch "\
   keyword monitor DP-2,1920x1080@165,-1000x1000,1;\
+  keyword windowrulev2 opacity 1.0 override 1.0 override,class:.*
+  keyword general:gaps_out 0;\
   keyword animations:enabled 0;\
   keyword decoration:drop_shadow 0;\
   keyword decoration:blur:enabled 0;\
@@ -14,7 +16,6 @@ hyprctl --batch "\
   keyword decoration:dim_inactive 0;"
 
 #  keyword general:gaps_in 0;\
-#  keyword general:gaps_out 0;\
 
 # Run Steam with Gamemode
 gamemoderun steam-runtime $@
