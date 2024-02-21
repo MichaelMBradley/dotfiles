d21e32a97efcddf47eba30d6d72c5b7e22a90b87
Michael Bradley <michaelmbrad@gmail.com>
2024-02-21 15:36:25 -0500

    Fix errors

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -2,11 +2,17 @@
 
 if [ "$(uname -n)" == "chonk" ]; then
   hyprctl dispatch workspace 3
-  hyprctl dispatch exec kitty
+  hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-1"
+  ydotool mousemove -x 90 -y 0
+  sleep 0.5
   hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-2"
+  ydotool mousemove -x 0 -y 90
+  sleep 0.5
   hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-3"
-  sleep 1
-  kitty @ --to 'unix:/tmp/kitty-command-2' send-text "c\n"
+  sleep 0.5
+  kitty @ --to 'unix:/tmp/kitty-command-1' send-text "c\n"
   kitty @ --to 'unix:/tmp/kitty-command-3' send-text "btop\n"
+  sleep 0.5
+  hyprctl dispatch resizeactive 0 -290
 fi
 
