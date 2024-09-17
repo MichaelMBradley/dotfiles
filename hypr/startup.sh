e99007d6e58b324d14d8577d2841c139e74ebdc9
Michael Bradley <michaelmbrad@gmail.com>
2024-09-16 22:14:18 -0400

    Kitty behaviour updates

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-if [ "$(uname -n)" == "chonk" ]; then
+if [ "$(uname -n)" = "chonk" ]; then
   hyprctl dispatch workspace 3
   ydotool mousemove -x 90 -y 0
   hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-1"
@@ -10,10 +10,9 @@ if [ "$(uname -n)" == "chonk" ]; then
   sleep 0.5
   hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-3"
   sleep 0.5
-  kitty @ --to 'unix:/tmp/kitty-command-1' send-text "c\n"
+  kitty @ --to 'unix:/tmp/kitty-command-2' send-text "c\n"
   kitty @ --to 'unix:/tmp/kitty-command-3' send-text "btop\n"
   sleep 0.5
   hyprctl dispatch resizeactive 0 -270
   hyprctl dispatch workspace 1
 fi
-
