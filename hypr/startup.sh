8f3488d22ec27339d635d2eaaa8c2664bc697b0b
Michael Bradley <michaelmbrad@gmail.com>
2024-12-01 19:16:01 -0500

    Update to latest kitty config

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -3,12 +3,12 @@
 if [ "$(uname -n)" = "chonk" ]; then
   hyprctl dispatch workspace 3
   ydotool mousemove -x 90 -y 0
-  hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-1"
+  hyprctl dispatch -- exec kitty --listen-on "unix:/tmp/kitty-command-1"
   sleep 0.5
-  hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-2"
+  hyprctl dispatch -- exec kitty --listen-on "unix:/tmp/kitty-command-2"
   ydotool mousemove -x 0 -y 90
   sleep 0.5
-  hyprctl dispatch -- exec kitty -o allow_remote_control="yes" --listen-on "unix:/tmp/kitty-command-3"
+  hyprctl dispatch -- exec kitty --listen-on "unix:/tmp/kitty-command-3"
   sleep 0.5
   kitty @ --to 'unix:/tmp/kitty-command-2' send-text "c\n"
   kitty @ --to 'unix:/tmp/kitty-command-3' send-text "btop\n"
