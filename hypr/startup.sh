9e1abced0ebe635312e0064cac9c5dad53b0f3b0
Michael Bradley <michaelmbrad@gmail.com>
2024-12-22 03:14:51 -0500

    Update chonk monitor layout

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -1,18 +1,1 @@
 #!/bin/env sh
-
-if [ "$(uname -n)" = "chonk" ]; then
-  hyprctl dispatch workspace 3
-  ydotool mousemove -x 90 -y 0
-  hyprctl dispatch -- exec kitty --listen-on "unix:/tmp/kitty-command-1"
-  sleep 0.5
-  hyprctl dispatch -- exec kitty --listen-on "unix:/tmp/kitty-command-2"
-  ydotool mousemove -x 0 -y 90
-  sleep 0.5
-  hyprctl dispatch -- exec kitty --listen-on "unix:/tmp/kitty-command-3"
-  sleep 0.5
-  kitty @ --to 'unix:/tmp/kitty-command-2' send-text "c\n"
-  kitty @ --to 'unix:/tmp/kitty-command-3' send-text "btop\n"
-  sleep 0.5
-  hyprctl dispatch resizeactive 0 -270
-  hyprctl dispatch workspace 1
-fi
